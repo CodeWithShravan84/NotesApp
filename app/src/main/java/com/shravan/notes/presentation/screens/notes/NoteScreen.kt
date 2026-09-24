46ce6eff22c90fe145b57e65675d199e224794f0
CodeWithShravan84 <codewithshravan84@gmail.com>
2026-09-24 13:19:32 +0530

    UI update

--- app/src/main/java/com/shravan/notes/presentation/screens/notes/NoteScreen.kt
+++ app/src/main/java/com/shravan/notes/presentation/screens/notes/NoteScreen.kt
@@ -1,30 +1,36 @@
 package com.shravan.notes.presentation.screens.notes
 
-import androidx.compose.foundation.ScrollState
+import android.R
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.shravan.notes.domain.Note
+import com.shravan.notes.presentation.ui.theme.Green
+import com.shravan.notes.presentation.ui.theme.Yellow200
 
 @Composable
 fun NoteScreen(
@@ -38,6 +44,20 @@ fun NoteScreen(
             .padding(48.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        item{
+            Title(text = "All Notes")
+        }
+        item {
+            SearchBar(
+                query = state.query,
+                onQueryChange = {
+                    viewModel.processCommand(NotesCommand.InputSearchQuery(it))
+                }
+            )
+        }
+        item {
+            Subtitle(text = "Pinned")
+        }
         item {
             LazyRow (
                 modifier = modifier.fillMaxWidth(),
@@ -47,42 +67,143 @@ fun NoteScreen(
                     items = state.pinnedNotes,
                     key = { it.id }
                 ){ note ->
-                    NotesCards(
+                    NoteCards(
                         note = note,
                         onNoteClick = {
+                            viewModel.processCommand(NotesCommand.EditNote(it))
+                        },
+                        onDoubleClick = {
+                            viewModel.processCommand(NotesCommand.DeleteNote(it.id))
+                        },
+                        onLongClick = {
                             viewModel.processCommand(NotesCommand.SwitchPinnedStatus(it.id))
-                        }
+                        },
+                        backgroundColor = Yellow200
                     )
                 }
             }
         }
 
+        item {
+            Subtitle(text = "Others")
+        }
         items(
             items = state.otherNotes ,
             key = { it.id }
         ) { note ->
-            NotesCards(
+            NoteCards(
+                modifier = Modifier.fillMaxWidth(),
                 note = note,
                 onNoteClick = {
+                    viewModel.processCommand(NotesCommand.EditNote(it))
+                },
+                onDoubleClick = {
+                    viewModel.processCommand(NotesCommand.DeleteNote(it.id))
+                },
+                onLongClick = {
                     viewModel.processCommand(NotesCommand.SwitchPinnedStatus(it.id))
-                }
+                },
+                backgroundColor = Green
             )
         }
     }
 }
 
-
 @Composable
-fun NotesCards(
+private fun Title(
     modifier: Modifier = Modifier,
-    note: Note,
-    onNoteClick: (Note) -> Unit
+    text: String
 ){
     Text(
-        modifier = modifier.clickable{
-            onNoteClick(note)
-        },
-        text = "${note.title} - ${note.content}",
-        fontSize = 24.sp
+        modifier = modifier,
+        text = text,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onBackground
     )
+}
+
+@Composable
+private fun SearchBar(
+    modifier: Modifier = Modifier,
+    query: String,
+    onQueryChange: (String) -> Unit
+){
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = query,
+        onValueChange = onQueryChange,
+        placeholder = {
+            Text(
+                text = "Search...",
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Notes"
+            )
+        },
+        shape = RoundedCornerShape(10.dp)
+    )
+}
+
+@Composable
+private fun Subtitle(
+    modifier: Modifier = Modifier,
+    text: String
+){
+    Text(
+        modifier = Modifier,
+        text = text,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    )
+}
+
+@Composable
+fun NoteCards(
+    modifier: Modifier = Modifier,
+    note: Note,
+    backgroundColor: Color,
+    onNoteClick: (Note) -> Unit,
+    onLongClick: (Note) -> Unit,
+    onDoubleClick: (Note) -> Unit
+){
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(backgroundColor)
+            .combinedClickable(
+                onClick = {
+                    onNoteClick(note)
+                },
+                onLongClick = {
+                    onLongClick(note)
+                },
+                onDoubleClick = {
+                    onDoubleClick(note)
+                }
+            )
+    ) {
+        Text(
+        text = note.title,
+        fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.onSurface
+    )
+        Text(
+            text = note.updatedAt.toString(),
+            fontSize = 12.sp,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Text(
+            text = note.content,
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.onSurface,
+            fontWeight = FontWeight.Medium
+        )
+    }
 }
